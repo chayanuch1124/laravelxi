a34542e9882be055d978f8c9490ac36c5788b46f
chayanuch1124 <chayanuch1124@gmail.com>
2024-12-11 20:20:28 +0700

    WEEK4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -57,16 +58,16 @@ Route::get("/gallery/cat", function () {
     return view("test/cat", compact("cat"));
 });
 
-Route::get("/teacher" , function (){
-	return view("teacher");
+Route::get("/teacher", function () {
+    return view("teacher");
 });
 
-Route::get("/student" , function (){
-	return view("student");
+Route::get("/student", function () {
+    return view("student");
 });
 
-Route::get("/theme" , function (){
-	return view("theme");
+Route::get("/theme", function () {
+    return view("theme");
 });
 
 Route::get('/active/index', function () {
@@ -97,3 +98,44 @@ Route::get('/active/blog', function () {
 Route::get('/active/contact', function () {
     return view('active/contact');
 })->name('contact');
+
+/* week4 */
+Route::get('/test', function () {
+    return view('test');
+})->name('test');
+
+// ค่อนข้างเปลืองพื้นที่
+Route::get('/coronavirus', function () {
+    $reports = [
+        (object) ["country" => "China", "date" => "2020-04-19", "total" => "2765", "active" => "790", "death" => "47", "recovered" => "1928"],
+        (object) ["country" => "China", "date" => "2020-04-18", "total" => "2733", "active" => "899", "death" => "47", "recovered" => "1787"],
+        (object) ["country" => "Thailand", "date" => "2020-04-17", "total" => "2700", "active" => "964", "death" => "47", "recovered" => "1689"],
+        (object) ["country" => "Thailand", "date" => "2020-04-16", "total" => "2672", "active" => "1033", "death" => "46", "recovered" => "1593"],
+        (object) ["country" => "Thailand", "date" => "2020-04-15", "total" => "2643", "active" => "1103", "death" => "43", "recovered" => "1497"],
+    ];
+    return view("coronavirus", compact("reports"));
+})->name('coronavirus');
+
+Route::get('/active/teacher', function () {
+    $teachers = json_decode(file_get_contents('https://raw.githubusercontent.com/arc6828/laravel8/main/public/json/teachers.json'));
+    return view("active.teacher", compact("teachers"));
+})->name('active.teacher');
+
+// Route::get('/category/sport', function () {
+//     return "<h1>This is sport Category Page</h1>";
+// });
+// Route::get('/category/politic', function () {
+//     return "<h1>This is politic Category Page</h1>";
+// });
+// Route::get('/category/entertain', function () {
+//     return "<h1>This is entertain Category Page</h1>";
+// });
+// Route::get('/category/auto', function () {
+//     return "<h1>This is auto Category Page</h1>";
+// });
+
+
+Route::get('/category/sport', [CategoryController::class, "sport"]);
+Route::get('/category/politic', [CategoryController::class, "politic"]);
+Route::get('/category/entertain', [CategoryController::class, "entertain"]);
+Route::get('/category/auto', [CategoryController::class, "auto"]);
